120c2dc5ae93799223b391ad875cc5fb643adae7
yelyzaveta-hrybniak-sa49 <yelyzaveta.hrybniak.sa49@gmail.com>
2020-10-21 21:54:20 +0300

    initial

---
+++ lab1/lab1.mk
@@ -0,0 +1,128 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=lab1
+ConfigurationName      :=Debug
+WorkspacePath          :=C:/Users/Lisa/Desktop/pr1
+ProjectPath            :=C:/Users/Lisa/Desktop/pr1/lab1
+IntermediateDirectory  :=./Debug
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=Lisa
+Date                   :=29/09/2020
+CodeLitePath           :="C:/Program Files/CodeLite"
+LinkerName             :=C:/TDM-GCC-64/bin/g++.exe
+SharedObjectLinkerName :=C:/TDM-GCC-64/bin/g++.exe -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="lab1.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=makedir
+RcCmpOptions           := 
+RcCompilerName         :=C:/TDM-GCC-64/bin/windres.exe
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := C:/TDM-GCC-64/bin/ar.exe rcu
+CXX      := C:/TDM-GCC-64/bin/g++.exe
+CC       := C:/TDM-GCC-64/bin/gcc.exe
+CXXFLAGS :=  -g -O0 -std=c++11 -Wall $(Preprocessors)
+CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := C:/TDM-GCC-64/bin/as.exe
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=C:\Program Files\CodeLite
+Objects0=$(IntermediateDirectory)/debug.cpp$(ObjectSuffix) $(IntermediateDirectory)/alloc.cpp$(ObjectSuffix) $(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IntermediateDirectory)/auto_test.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "./Debug"
+
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "./Debug"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/debug.cpp$(ObjectSuffix): debug.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/debug.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/debug.cpp$(DependSuffix) -MM debug.cpp
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Lisa/Desktop/pr1/lab1/debug.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/debug.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/debug.cpp$(PreprocessSuffix): debug.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/debug.cpp$(PreprocessSuffix) debug.cpp
+
+$(IntermediateDirectory)/alloc.cpp$(ObjectSuffix): alloc.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/alloc.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/alloc.cpp$(DependSuffix) -MM alloc.cpp
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Lisa/Desktop/pr1/lab1/alloc.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/alloc.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/alloc.cpp$(PreprocessSuffix): alloc.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/alloc.cpp$(PreprocessSuffix) alloc.cpp
+
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Lisa/Desktop/pr1/lab1/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+
+$(IntermediateDirectory)/auto_test.cpp$(ObjectSuffix): auto_test.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/auto_test.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/auto_test.cpp$(DependSuffix) -MM auto_test.cpp
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Lisa/Desktop/pr1/lab1/auto_test.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/auto_test.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/auto_test.cpp$(PreprocessSuffix): auto_test.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/auto_test.cpp$(PreprocessSuffix) auto_test.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r ./Debug/
+
+
