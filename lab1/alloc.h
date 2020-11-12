c098f75beed0f90adb53c82d54a2cdb01d1da3c2
hrybnyak <hrybnyak.y@gmail.com>
2020-11-12 09:17:35 +0200

    remove comments

--- lab1/alloc.h
+++ lab1/alloc.h
@@ -20,22 +20,14 @@ class Allocator {
   Allocator(size_t size);
   ~Allocator();
 
-  // if available - allocates size bytes of memory from pool 
-  // and returns a pointer to the first byte or returns
-  // nullptr otherwise
   void *mem_alloc(size_t size);
 
-  // Frees the memory pointed by ptr and also defragments
-  // memory chunks
   void mem_free(void *ptr);
 
-  // Extends or shrinks memory chunk pointed by ptr if available
-  // or allocates new chunk of size and copies the previously pointed
-  // data there while calling mem_free on the current ptr address
   void *mem_realloc(void *ptr, size_t size);
 
   void mem_dump();
 };
 }
 
-#endif //LAB1_ALLOCATOR_ALLOC_H
+#endif 
